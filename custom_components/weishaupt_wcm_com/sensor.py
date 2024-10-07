a1bf8ec9fd6015983b4fd8428cc649973af715b9
zobe123 <manuel.zobl@gmx.at>
2024-10-07 09:17:32 +0200

    Update sensor.py

--- custom_components/weishaupt_wcm_com/sensor.py
+++ custom_components/weishaupt_wcm_com/sensor.py
@@ -1,82 +1,86 @@
 """Platform for sensor integration."""
-from homeassistant.const import TEMP_CELSIUS
-from homeassistant.helpers.entity import Entity
 import logging
-from datetime import timedelta, datetime
+from datetime import timedelta
 
-from .const import DOMAIN
-from .const import NAME_PREFIX
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import UnitOfTemperature
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.config_entries import ConfigEntry
 
-from .const import OIL_CONSUMPTION_KEY
-from .const import OUTSIDE_TEMPERATURE_KEY
-from .const import LOAD_SETTING_KEY
-from .const import WARM_WATER_TEMPERATURE_KEY 
-from .const import FLOW_TEMPERATURE_KEY
-from .const import FLUE_GAS_TEMPERATURE_KEY
-
-from . import WeishauptBaseEntity
-
-
-
-SENSOR_TYPES = {
-    "oil_meter"
-}
+from .const import (
+    DOMAIN,
+    NAME_PREFIX,
+    PARAMETERS,
+    ERROR_CODE_KEY,
+    ERROR_CODE_MAP,
+    WARNING_CODE_MAP,
+    OPERATION_MODE_MAP,
+    OPERATION_PHASE_MAP,
+)
+from .base_entity import WeishauptBaseEntity  # Importieren Sie die Basisklasse von base_entity.py
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the sensor platform."""
-    # We only want this platform to be set up via discovery.
-    if discovery_info is None:
-        return
-    add_entities(
-        [
-            WeishauptSensor(hass, config, OIL_CONSUMPTION_KEY, "l"),
-            WeishauptSensor(hass, config, OUTSIDE_TEMPERATURE_KEY, "°C"),
-            WeishauptSensor(hass, config, LOAD_SETTING_KEY, "kW"),
-            WeishauptSensor(hass, config, WARM_WATER_TEMPERATURE_KEY, "°C"),
-            WeishauptSensor(hass, config, FLOW_TEMPERATURE_KEY, "°C"),
-            WeishauptSensor(hass, config, FLUE_GAS_TEMPERATURE_KEY, "°C")
-        ]
-    )
+    api = hass.data[DOMAIN][entry.entry_id]["api"]
+    scan_interval = hass.data[DOMAIN][entry.entry_id]["scan_interval"]
 
+    sensors = []
+    for param in PARAMETERS:
+        sensor_name = param["name"]
+        unit = UnitOfTemperature.CELSIUS if param["type"] == "temperature" else None
+        sensors.append(WeishauptSensor(api, sensor_name, unit, scan_interval))
 
-class WeishauptSensor(WeishauptBaseEntity):
-    """Representation of a Sensor."""
+    async_add_entities(sensors)
 
-    def __init__(self, hass, config, sensor_name, sensor_unit):
-        super().__init__(hass, config)
+class WeishauptSensor(WeishauptBaseEntity, SensorEntity):
+    """Representation of a Weishaupt Sensor."""
+
+    def __init__(self, api, sensor_name, unit, scan_interval):
         """Initialize the sensor."""
-        self._state = None
-        self._data = {}
-        self._config = config
-
-        self._name = sensor_name
-        self._unit = sensor_unit
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return NAME_PREFIX + self._name
+        super().__init__(api)
+        self._sensor_name = sensor_name
+        self._attr_name = f"{NAME_PREFIX}{self._sensor_name}"
+        self._attr_native_unit_of_measurement = unit
+        self._attr_unique_id = f"{DOMAIN}_{self._sensor_name.lower().replace(' ', '_')}"
+        self._scan_interval = timedelta(seconds=scan_interval)
 
     @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
+    def scan_interval(self):
+        """Return the scan interval."""
+        return self._scan_interval
 
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return self._unit
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        await self.hass.async_add_executor_job(self.api.update)
+        data = self.api.data
+        try:
+            value = data.get(self._sensor_name)
+            if value is None:
+                self._attr_native_value = None
+                _LOGGER.debug(f"Data for {self._sensor_name} not found")
+                return
 
-    def update(self):
-        """Fetch new state data for the sensor.
+            # Spezielle Verarbeitung für bestimmte Sensoren
+            if self._sensor_name == ERROR_CODE_KEY:
+                self._attr_native_value = self.api.process_codes(value)
+            elif self._sensor_name == "Operation Mode":
+                self._attr_native_value = OPERATION_MODE_MAP.get(value, f"Unbekannter Modus ({value})")
+            elif self._sensor_name == "Operation Phase":
+                self._attr_native_value = OPERATION_PHASE_MAP.get(value, f"Unbekannte Phase ({value})")
+            elif param_type := next((p["type"] for p in PARAMETERS if p["name"] == self._sensor_name), None):
+                if param_type == "binary":
+                    self._attr_native_value = "Ein" if value else "Aus"
+                elif param_type == "value":
+                    self._attr_native_value = value
+                elif param_type == "temperature":
+                    self._attr_native_value = value
+                else:
+                    self._attr_native_value = value
+            else:
+                self._attr_native_value = value
 
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        _LOGGER.debug("Updating Sensor")
-        super().update()
-        try: 
-            self._state = self.api().getData()[self._name]
-        except:
-            self._state = None
+        except Exception as e:
+            _LOGGER.error(f"Error updating sensor {self._sensor_name}: {e}")
+            self._attr_native_value = None
